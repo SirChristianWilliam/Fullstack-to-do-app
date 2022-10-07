5de95524772f5b532b07ea3dd3ef9c45db7c3cb0
Chris Maki <c.william.developer@gmail.com>
2022-10-07 13:42:45 -0500

    testing link

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+app.use(bodyParser.json());
