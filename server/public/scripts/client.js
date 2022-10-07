2faf1629f7685d5a5e1aee611c36b82838a590f6
Chris Maki <c.william.developer@gmail.com>
2022-10-07 13:58:10 -0500

    reset

--- server/public/scripts/client.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(onReady)
-
-function onReady() {
-   
-    console.log('Jquery ready, in client.js');
-}
--- server/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = 5000;
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
-app.use(bodyParser.json());
