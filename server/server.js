2f1fa032eec19ff0a65335758b4e91f85eae496e
Chris Maki <c.william.developer@gmail.com>
2022-10-07 16:46:08 -0500

    server is online

--- server/server.js
+++ server/server.js
@@ -11,4 +11,8 @@ app.use(bodyParser.json());
 // Serve "static assets" (html, css, client-side js)
 // from the server/public folder
 app.use(express.static('server/public'));
- 
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log('up and running on port', PORT);
+});
